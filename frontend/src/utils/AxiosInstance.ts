bc730e3f2bba9bcbf2e9c87aab004996af0e6c7d
Saurav Jha <srvtj629@gmail.com>
2025-05-28 17:00:27 +0530

    domains changed

--- frontend/src/utils/AxiosInstance.ts
+++ frontend/src/utils/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api/v1",
+  baseURL: "https://codewarrior-770699899269.asia-south1.run.app/api/v1",
   withCredentials: true, 
 });
 
