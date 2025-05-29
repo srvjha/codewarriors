84ac6da02444271b9b8f344980ca712f575d9a5d
Saurav Jha <srvtj629@gmail.com>
2025-05-30 01:25:19 +0530

    bug fixed

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -9,7 +9,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
-    <GoogleOAuthProvider clientId={import.meta.env.GOOGLE_CLIENT_ID}>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
